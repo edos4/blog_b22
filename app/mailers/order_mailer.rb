4a1307878738f0a9afd9068d25c3afc9192178e2
Ed Salinas <edsalinas7b@yahoo.com>
2022-12-16 17:59:34 +0800

    Setup mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,9 @@
 class OrderMailer < ApplicationMailer
+  def new_order_email
+    @order = params[:order]
+
+    user = User.first
+
+    mail(to: user.email, subject: "You got a new order!")
+  end
 end
